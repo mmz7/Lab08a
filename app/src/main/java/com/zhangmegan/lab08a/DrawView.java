e751f2869e1ea23bbedab104019fbb0a7224a486
mmz7 <mmzhang711@gmail.com>
2021-11-10 14:10:52 -0500

    animate?

--- app/src/main/java/com/zhangmegan/lab08a/DrawView.java
+++ app/src/main/java/com/zhangmegan/lab08a/DrawView.java
@@ -1,11 +1,15 @@
 package com.zhangmegan.lab08a;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -18,6 +22,9 @@ public class DrawView extends View {
     int linex = 0;
     int liney = 600;
     int ovalWidth = 160;
+    int y = 400, dY = 1;
+    int x = getWidth(), dX = 2;
+
 
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -26,36 +33,46 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        //start
+        Paint star = new Paint();
+        star.setColor(Color.WHITE);
+        canvas.drawCircle(x, y, 4, star);
+
+        // street
         Path myPath = new Path();
         myPath.moveTo(0,getHeight());
-        myPath.lineTo(450, getHeight()-1200);
-        myPath.lineTo(getWidth()-450, getHeight()-1200);
+        myPath.lineTo(400, getHeight()-1000);
+        myPath.lineTo(getWidth()-400, getHeight()-1000);
         myPath.lineTo(getWidth(), getHeight());
         Paint street = new Paint();
         street.setColor(Color.parseColor("#9c9491"));
         canvas.drawPath(myPath, street);
 
+        // walls
         Paint building = new Paint();
         building.setColor(Color.parseColor("#9c5240"));
         myPath.reset();
         myPath.moveTo(0, getHeight());
         myPath.lineTo(0, 0);
-        myPath.lineTo(450, 0);
-        myPath.lineTo(450, getHeight()-1200);
+        myPath.lineTo(400, 0);
+        myPath.lineTo(400, getHeight()-1000);
         canvas.drawPath(myPath, building);
 
         myPath.reset();
         myPath.moveTo(getWidth(), getHeight());
         myPath.lineTo(getWidth(), 0);
-        myPath.lineTo(getWidth()-450, 0);
-        myPath.lineTo(getWidth()-450, getHeight()-1200);
+        myPath.lineTo(getWidth()-400, 0);
+        myPath.lineTo(getWidth()-400, getHeight()-1000);
         canvas.drawPath(myPath, building);
 
-        myPath.reset();
-        myPath.moveTo(20, getHeight()-300);
-
-
-        canvas.drawPath(myPath, new Paint());
+        // door
+//        myPath.reset();
+//        myPath.moveTo(100, getHeight()-270);
+//        myPath.lineTo(100, getHeight()-800);
+//        myPath.lineTo(240, getHeight()-1050);
+//        myPath.lineTo(240, getHeight()-650);
+//        canvas.drawPath(myPath, new Paint());
 
         red.setColor(Color.parseColor("#f24e30"));
         gold.setColor(Color.parseColor("#ffc929"));
@@ -82,5 +99,11 @@ public class DrawView extends View {
         canvas.drawRect(r3, gold);
         canvas.drawRect(r4, gold);
         canvas.drawRect(r5, gold);
+
+        y-= dY;
+        x+= dX;
+        invalidate();
+
     }
+
 }
